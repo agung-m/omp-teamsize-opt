7b68906c9b810c272dbfff9dfa20fa66c98fbe93
System User <xiaoxiong@phi.aq.sc.isc.tohoku.ac.jp>
2020-08-09 15:46:44 +0900

    change params

--- run_dubinsky.sh
+++ run_dubinsky.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 #TIME_LIMIT=0.000001
-TIME_LIMIT=0.0000001
+TIME_LIMIT=0.00001
 
 # build with the new team sizes
+
 #rm -f bin/barneshut-p
+make clean
 make --silent -f Makefile-parallel
+#make --silent -f Makefile-parallel
 
 # run
 bin/barneshut-p datasets/dubinski $TIME_LIMIT
